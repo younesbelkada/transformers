b50e7a73f46b3cd53eacc9b3f7a271d353a6376d
younesbelkada <younesbelkada@gmail.com>
2022-05-03 17:49:42 +0200

    add new tests

--- tests/bigscience176b/test_logits_cu115.py
+++ tests/bigscience176b/test_logits_cu115.py
@@ -12,20 +12,23 @@ def torch_assert_equal(actual, expected, **kwargs):
     else:
         return torch.allclose(actual, expected, rtol=0.0, atol=0.0)
 
-path_meg_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/megatron-logits"
-path_tr_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/transformers-logits"
+path_meg_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/logits_parallel_rank_0.pt"
+path_tr_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/logits_1_tr_apex_cu115.pt"
 
-input_linear_tr = torch.load(os.path.join(path_tr_logits, "context_layer_after_1.pt"), map_location=device)
-input_linear_mgs = torch.load(os.path.join(path_meg_logits, "input_row_mlp_rank_0.pt"), map_location=device)
+# input_linear_tr = torch.load(os.path.join(path_tr_logits, "context_layer_after_1.pt"), map_location=device)
+# input_linear_mgs = torch.load(os.path.join(path_meg_logits, "input_row_mlp_rank_0.pt"), map_location=device)
 
-weight_tr = torch.load(os.path.join(path_tr_logits, "dense_weight_1.pt"), map_location=device)
-weight_meg = torch.load(os.path.join(path_meg_logits, "weight_mlp_rank_0.pt"), map_location=device)
+# weight_tr = torch.load(os.path.join(path_tr_logits, "dense_weight_1.pt"), map_location=device)
+# weight_meg = torch.load(os.path.join(path_meg_logits, "weight_mlp_rank_0.pt"), map_location=device)
 
-output_tr = torch.load(os.path.join(path_tr_logits, "output_attn_layer_1.pt"), map_location=device)
-output_meg = torch.load(os.path.join(path_meg_logits, "output_layer_1_rank_0.pt"), map_location=device)
+# output_tr = torch.load(os.path.join(path_tr_logits, "output_attn_layer_1.pt"), map_location=device)
+# output_meg = torch.load(os.path.join(path_meg_logits, "output_layer_1_rank_0.pt"), map_location=device)
 
-torch_assert_equal(F.linear(input_linear_tr, weight_tr), F.linear(input_linear_mgs, weight_meg))
-torch_assert_equal(F.linear(input_linear_tr, weight_tr), output_meg)
-torch_assert_equal(F.linear(input_linear_tr, weight_tr), output_tr)
+# torch_assert_equal(F.linear(input_linear_tr, weight_tr), F.linear(input_linear_mgs, weight_meg))
+# torch_assert_equal(F.linear(input_linear_tr, weight_tr), output_meg)
+# torch_assert_equal(F.linear(input_linear_tr, weight_tr), output_tr)
 
+meg_logits = torch.load(path_meg_logits, map_location=device)
+tr_logits = torch.load(path_tr_logits, map_location=device)
 
+torch_assert_equal(meg_logits, tr_logits)
