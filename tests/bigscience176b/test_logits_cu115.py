c3e66fef33724feba699759601324d48bdb41a90
younesbelkada <younesbelkada@gmail.com>
2022-05-03 15:51:02 +0200

    fix issue

--- tests/bigscience176b/test_logits_cu115.py
+++ tests/bigscience176b/test_logits_cu115.py
@@ -15,14 +15,14 @@ def torch_assert_equal(actual, expected, **kwargs):
 path_meg_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/megatron-logits"
 path_tr_logits = "/gpfswork/rech/six/uan68tv/data/tensors_to_test/transformers-logits"
 
-input_linear_tr = os.path.join(path_tr_logits, "context_layer_after_1.pt", map_location=device)
-input_linear_mgs = os.path.join(path_meg_logits, "input_row_mlp_rank_0.pt", map_location=device)
+input_linear_tr = torch.load(os.path.join(path_tr_logits, "context_layer_after_1.pt"), map_location=device)
+input_linear_mgs = torch.load(os.path.join(path_meg_logits, "input_row_mlp_rank_0.pt"), map_location=device)
 
-weight_tr = os.path.join(path_tr_logits, "dense_weight_1.pt", map_location=device)
-weight_meg = os.path.join(path_meg_logits, "weight_mlp_rank_0.pt", map_location=device)
+weight_tr = torch.load(os.path.join(path_tr_logits, "dense_weight_1.pt"), map_location=device)
+weight_meg = torch.load(os.path.join(path_meg_logits, "weight_mlp_rank_0.pt"), map_location=device)
 
-output_tr = os.path.join(path_tr_logits, "output_attn_layer_1.pt", map_location=device)
-output_meg = os.path.join(path_meg_logits, "output_layer_1_rank_0.pt", map_location=device)
+output_tr = torch.load(os.path.join(path_tr_logits, "output_attn_layer_1.pt"), map_location=device)
+output_meg = torch.load(os.path.join(path_meg_logits, "output_layer_1_rank_0.pt"), map_location=device)
 
 torch_assert_equal(F.linear(input_linear_tr, weight_tr), F.linear(input_linear_mgs, weight_meg))
 torch_assert_equal(F.linear(input_linear_tr, weight_tr), output_meg)
