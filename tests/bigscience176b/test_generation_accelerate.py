5765b751c551071ade59212cd632f1add1d7495c
younesbelkada <younesbelkada@gmail.com>
2022-05-06 13:59:25 +0200

    change file name

--- tests/bigscience176b/test_generation_accelerate.py
+++ tests/bigscience176b/test_generation_accelerate.py
@@ -15,8 +15,8 @@ print("successfully loaded model")
 model.tie_weights()
 # Determine a device map that maximizes the available GPUs, you can also write your own.
 # If using a different model, adjust `T5Block` to the proper class (for instance `"GPTJBlock"`)
-# device_map = infer_auto_device_map(model, no_split_module_classes=["BigScience176BBlock"])
-device_map = infer_auto_device_map(model)
+device_map = infer_auto_device_map(model, no_split_module_classes=["BigScience176BBlock"])
+# device_map = infer_auto_device_map(model)
 # Load the sharded checkpoint inside the model. This will load each part of the model on the device specified by `device_map`
 load_sharded_checkpoint_in_model(model, model_name, device_map=device_map)
 # This will make that model that leaves on several different devices just work.
