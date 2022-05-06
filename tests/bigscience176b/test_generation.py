8fcdb090abd795455f474d66657e2bf00443d1f4
younesbelkada <younesbelkada@gmail.com>
2022-05-06 11:37:07 +0200

    add to gpu

--- tests/bigscience176b/test_generation.py
+++ tests/bigscience176b/test_generation.py
@@ -5,6 +5,11 @@ model_name = "/gpfswork/rech/six/uan68tv/model-conversion/tr11e-350M-transformer
 config = AutoConfig.from_pretrained(model_name)
 
 model = BigScience176BLMHeadModel.from_pretrained(model_name, use_cache=False, low_cpu_mem_usage=True)
+
+device_map = {0:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]}
+model.parallelize(device_map)
+model.eval()
+
 tokenizer = AutoTokenizer.from_pretrained("bigscience-catalogue-data-dev/byte-level-bpe-tokenizer-no-norm-250k-whitespace-and-eos-regex-alpha-v3-dedup-lines-articles")
 
 input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='pt')
