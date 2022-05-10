af0662596c6c5f6becaf008127c6db7dfe3afda5
younesbelkada <younesbelkada@gmail.com>
2022-05-10 13:27:34 +0200

    try with that

--- src/transformers/models/bigscience176b/modeling_bigscience176b.py
+++ src/transformers/models/bigscience176b/modeling_bigscience176b.py
@@ -896,7 +896,7 @@ class BigScience176BModel(BigScience176BPreTrainedModel):
 )
 class BigScience176BLMHeadModel(BigScience176BPreTrainedModel):
     # _keys_to_ignore_on_load_missing = [r"attn.masked_bias", r"attn.bias", r"lm_head.weight"]
-    _keys_to_ignore_on_load_missing = [r"transformer.h.*.self_attention.scale_mask_softmax.causal_mask"]
+    _keys_to_ignore_on_load_missing = [r"h.*.self_attention.scale_mask_softmax.causal_mask"]
 
     def __init__(self, config):
         super().__init__(config)
